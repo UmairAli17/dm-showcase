54258cf876a33b7088147c4fea12e22fd2aed399
Umair Ali <umairali17@hotmail.co.uk>
2017-05-04 21:32:51 +0100

    Last SVG Implemented

--- src/scripts/svg.js
+++ src/scripts/svg.js
@@ -21,4 +21,20 @@ $(document).ready(function() {
 
     var scale = $('#tipping');
     scale.addClass('pivot');
+
+    var movindHand = $('#moving_x5F_hand');
+    movindHand.addClass('rotateHand');
+
+
+    //Traffic Lights
+     $('.lightBlink').each(function(i){
+        var ThisIt = $(this);
+        setTimeout(function(){
+          ThisIt.addClass('fadeInLights');
+        } , i * 3750);
+    
+    });
+
+     var pressure = $("#gaugeHand");
+     pressure.addClass('pressureGauge');
 });
